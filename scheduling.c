eb98144e6c58355c9f6721f9e61b7b2e88b10891
John Chen <johnchen77w@gmail.com>
2019-10-26 00:16:56 -0400

    Update scheduling.c

--- scheduling.c
+++ scheduling.c
@@ -13,24 +13,24 @@
 /*
     Main fcfs function
 */
-int fcfs(char **arr_fcfs, int pCounter)
+int fcfs(char **arr_fcfs, int pCounter, FILE *new_file)
 {
     int finalTime = 0;
     int totalTime = 0;
-    printf("            Order of selection by CPU:  \n\n");
-    printf("                ");
+    fprintf(new_file, "        Order of selection by CPU:  \n\n");
+    fprintf(new_file, "            ");
     for (int i = 0; i < pCounter; i++) {
-        printf("P%d ", i+1);
+        fprintf(new_file, "P%d ", i+1);
     }
-    printf("\n\n            Individual waiting times for each process: \n\n");
+    fprintf(new_file, "\n\n        Individual waiting times for each process: \n\n");
 
     for (int j = 0; j < pCounter; j++) {
-        printf("                P%d = %d\n", j+1, finalTime);
+        fprintf(new_file, "            P%d = %d\n", j+1, finalTime);
         finalTime = finalTime + atoi(arr_fcfs[j]);
         totalTime = totalTime + finalTime;
     }
     totalTime = totalTime - finalTime;
-    printf("\n            Average waiting time = %0.1f \n\n", (double)totalTime/pCounter);
+    fprintf(new_file, "\n        Average waiting time = %0.1f \n\n", (double)totalTime/pCounter);
     return 0;
 }
 
@@ -63,7 +63,7 @@ int sjf_cmp(const void *a, const void *b)
 /*
     Main sjf function, using qsort() to sort the array elements in ascending order
 */
-int sjf(char **arr_sjf, int pCounter)
+int sjf(char **arr_sjf, int pCounter, FILE *new_file)
 {
     char *temp;
     int finalTime = 0;
@@ -75,13 +75,13 @@ int sjf(char **arr_sjf, int pCounter)
         objects[i].index = i;
     }
     qsort(objects, pCounter, sizeof(objects[0]), sjf_cmp);
-    printf("        Order of selection by CPU:  \n\n");
-    printf("            ");
+    fprintf(new_file, "        Order of selection by CPU:  \n\n");
+    fprintf(new_file, "            ");
     for (int i = 0; i < pCounter; i++)
     {
-        printf("P%d ", objects[i].index + 1); //will give 1 2 0
+        fprintf(new_file,      "P%d ", objects[i].index + 1); //will give 1 2 0
     }
-    printf("\n\n        Individual waiting times for each process:   \n\n");
+    fprintf(new_file, "\n\n        Individual waiting times for each process:   \n\n");
     for (int i = 0; i < pCounter; i++)
     {
         for (int j = i + 1; j < pCounter; j++)
@@ -93,12 +93,12 @@ int sjf(char **arr_sjf, int pCounter)
                 arr_sjf[j] = temp;
             }
         }
-        printf("            P%d = %d\n", objects[i].index + 1, finalTime);
+        fprintf(new_file, "            P%d = %d\n", objects[i].index + 1, finalTime);
         finalTime = finalTime + atoi(arr_sjf[i]);
         totalTime = totalTime + finalTime;
     }
     totalTime = totalTime - finalTime;
-    printf("\n        Average waiting time = %0.1f \n\n", (double)totalTime/pCounter);
+    fprintf(new_file, "\n        Average waiting time = %0.1f \n\n", (double)totalTime/pCounter);
     return 0;
 }
 
@@ -106,17 +106,17 @@ int sjf(char **arr_sjf, int pCounter)
 /*
     Main rr function
 */
-int rr(char **arr_rr, int pCounter, int tq)
+int rr(char **arr_rr, int pCounter, int tq, FILE *new_file)
 {
     int finished = 0; // 0 = finished, 1 = not finished yet
     int process_remains;
     int pQueue [pCounter]; 
-    printf("            Order of selection by CPU:  \n\n");
-    printf("                ");
+    fprintf(new_file, "            Order of selection by CPU:  \n\n");
+    fprintf(new_file, "                ");
     for (int i = 0; i < pCounter; i++)
     {
         pQueue[i] = atoi(arr_rr[i]);
-        printf("P%d ", i + 1);
+        fprintf(new_file, "P%d ", i + 1);
     }
     while (finished == 0)
     {
@@ -126,7 +126,7 @@ int rr(char **arr_rr, int pCounter, int tq)
             if (pQueue[i] > tq)
             {
                 pQueue[i] = pQueue[i] - tq;
-                printf("P%d ", i + 1);
+                fprintf(new_file, "P%d ", i + 1);
             }
             else if (pQueue[i] < tq)
             {
@@ -142,15 +142,9 @@ int rr(char **arr_rr, int pCounter, int tq)
             }
         }
     }  
+    fprintf(new_file, "\n\n");
     return 0;
 }
-
-
-
-
-
-
-
 
 
 
@@ -163,6 +157,7 @@ int main(int argc, char const *argv[])
     char const* const fileName = argv[1];
     char line [BUFFER_MAX_LENGTH];
     FILE *file;
+    FILE *new_file;
     char *split;
     char delim[] = " ";
     char *lineArr[BUFFER_MAX_LENGTH];
@@ -171,58 +166,64 @@ int main(int argc, char const *argv[])
     file = fopen("cpu_scheduling_input_file.txt", "r");
     if (!file)
     {
-         fprintf(stderr, "error: could not open textfile: cpu_scheduling_input_file.txt\n");
-         return EXIT_FAILURE;
+        fprintf(stderr, "error: could not open textfile: cpu_scheduling_input_file.txt\n");
+        return EXIT_FAILURE;
     }
-    while (fgets(line, sizeof(line), file) != NULL) {
+    new_file = fopen("cpu_scheduling_output_file.txt", "a");
+    if (!new_file)
+    {
+        fprintf(stderr, "error: could not open textfile: cpu_scheduling_output_file.txt\n");
+        return EXIT_FAILURE;
+    }
+    while (fgets(line, sizeof(line), file) != NULL) 
+    {
         lineArr[lineCounter] = malloc(sizeof(line));
         strcpy(lineArr[lineCounter], line);
         lineCounter++;
     }
-
-    printf("\n- - - - - - - - - - - - S - T - A - R - T - - - - - - - - - - - - \n\n");
-    printf("%d ready queues have been read and stored\n\n", lineCounter);
-
-    for (size_t j = 0; j < lineCounter; j++) {
-        printf("\n- - - - - - - - - - - - S - P - A - C - E - - - - - - - - - - - - \n\n");
-        printf("Ready queue %zu\n\n", j+1);
+    fprintf(new_file, "- - - - - - - - - - - - - - - - - - - - -  S - T - A - R - T - - - - - - - - - - - - - - - - - - - - - \n\n");
+    fprintf(new_file, "%d ready queues have been read and stored\n", lineCounter);
+    for (size_t j = 0; j < lineCounter; j++) 
+    {
+        fprintf(new_file, "\n- - - - - - - - - - - - Q - U - E - U - E - - %d - - - - - - - - - - - - \n\n", j + 1);
         // first, count number of P's in one line to determine its number of processes
         int pCounter = 0;
-        for (size_t k = 0; k < strlen(lineArr[j]); k++) {
+        for (size_t k = 0; k < strlen(lineArr[j]); k++) 
+        {
             if(lineArr[j][k] == 'p')
             pCounter++;
         }
-        printf("    Number of processes: %d\n\n", pCounter);
-        // printf("    %s\n", lineArr[j]);
+        fprintf(new_file, "Number of processes: %d\n\n", pCounter);
         int splitCounter = 0;
         split = strtok (lineArr[j], delim);
-        while (split != NULL) {
+        while (split != NULL) 
+        {
             splitArr[splitCounter] = malloc(strlen(lineArr[j]));
             strcpy(splitArr[splitCounter], split);
             splitCounter++;
             split = strtok(NULL, delim);
         }
         timeQuantum = atoi(splitArr[3]);
-        printf("    Time quantum: %d\n\n", timeQuantum);
-
-
+        fprintf(new_file, "Time quantum: %d\n\n", timeQuantum);
         // another array corresponding to its process times
         int proCounter = 0;
-        for (int p = 5; p < splitCounter; p = p + 2) {
+        for (int p = 5; p < splitCounter; p = p + 2) 
+        {
         processArr[proCounter] = (char*)malloc(strlen(splitArr[p]) + 1);
         strcpy(processArr[proCounter], splitArr[p]);
         proCounter++;
         }
-        // printf("        Ready Queue %lu Applying FCFS Scheduling:\n\n", j+1);
-        // fcfs(processArr, proCounter);
-        // printf("    Ready Queue %zu Applying SJF Scheduling...\n\n", j+1);
-        // sjf(processArr, proCounter);
-        printf("    Ready Queue %zu Applying RR Scheduling...\n\n", j+1);
-        rr(processArr, proCounter, timeQuantum);
+        fprintf(new_file, "    Ready Queue %lu Applying FCFS Scheduling:\n\n", j+1);
+        fcfs(processArr, proCounter, new_file);
+        fprintf(new_file, "    Ready Queue %zu Applying RR Scheduling...\n\n", j+1);
+        rr(processArr, proCounter, timeQuantum, new_file);
+        fprintf(new_file, "    Ready Queue %zu Applying SJF Scheduling...\n\n", j+1);
+        sjf(processArr, proCounter, new_file);
+
     }
 
 
-
+    fclose(new_file);
     fclose(file);
 
     return 0;
